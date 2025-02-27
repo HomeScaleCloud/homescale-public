f73235631e8dbc4b2a8d4158b94b394a28f6e478
Maxwell Morris <max@morrislan.net>
2025-02-27 15:20:39 +0000

    more things cuz i was silly

--- infra/tofu/k8s_cluster.tf
+++ infra/tofu/k8s_cluster.tf
@@ -1,4 +1,4 @@
-resource "vultr_kubernetes" "uk_lon_1_cp" {
+resource "vultr_kubernetes" "uk_lon_1_mgmt" {
   region          = "lhr"
   label           = "uk-lon-1"
   version         = "v1.32.1+1"
@@ -8,6 +8,6 @@ resource "vultr_kubernetes" "uk_lon_1_cp" {
   node_pools {
     node_quantity = 2
     plan          = "vc2-2c-4gb"
-    label         = "uk-lon-1-cp-node"
+    label         = "uk-lon-1-mgmt-node"
   }
 }
