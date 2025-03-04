18e0b47ab5eca849673631459ea248122b3fc0ee
Maxwell Morris <max@morrislan.net>
2025-03-04 16:40:03 +0000

    things

--- infra/tofu/k8s_cluster.tf
+++ infra/tofu/k8s_cluster.tf
@@ -1,17 +1,3 @@
-resource "vultr_kubernetes" "uk_lon_1_mgmt" {
-  region          = "lhr"
-  label           = "uk-lon-1-mgmt"
-  version         = "v1.31.5+1"
-  enable_firewall = true
-
-
-  node_pools {
-    node_quantity = 2
-    plan          = "vc2-2c-4gb"
-    label         = "uk-lon-1-mgmt-node"
-  }
-}
-
 resource "digitalocean_kubernetes_cluster" "uk_lon_1_mgmt" {
   name    = "uk-lon-1-mgmt"
   region  = "lon1"
