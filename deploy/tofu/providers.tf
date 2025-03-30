2da36261968603d597246be98a97248e466760a2
Maxwell Morris <git@maxmorris.io>
2025-03-30 19:07:15 +0100

    initial modularity restructure

---
+++ deploy/tofu/providers.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+    vultr = {
+      source = "vultr/vultr"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+    kubernetes = {
+      source = "kubernetes"
+    }
+    helm = {
+      source = "helm"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
+}
+
+provider "vultr" {
+  api_key = var.vultr_token
+}
+
+provider "digitalocean" {
+  token             = var.digitalocean_token
+  spaces_access_id  = var.digitalocean_spaces_id
+  spaces_secret_key = var.digitalocean_spaces_key
+}
+
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.mgmt.endpoint
+  token = digitalocean_kubernetes_cluster.mgmt.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.mgmt.kube_config[0].cluster_ca_certificate
+  )
+}
+
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.mgmt.endpoint
+    token = digitalocean_kubernetes_cluster.mgmt.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.mgmt.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
