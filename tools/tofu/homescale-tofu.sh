e66264e85d1719821a0738037666c513866bcec2
Maxwell Morris <git@maxmorris.io>
2025-04-03 14:03:14 +0100

    fix

--- tools/tofu/homescale-tofu.sh
+++ tools/tofu/homescale-tofu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Wrapper script for OpenTofu with 1Password secrets
 
-export AWS_ACCESS_KEY_ID="op://github-actions/digitalocean/spaces-access-key-id"
-export AWS_SECRET_ACCESS_KEY="op://github-actions/digitalocean/spaces-secret-access-key" #pragma: allowlist secret
+export AWS_ACCESS_KEY_ID="op://common/digitalocean/spaces-access-key-id"
+export AWS_SECRET_ACCESS_KEY="op://common/digitalocean/spaces-secret-access-key" #pragma: allowlist secret
 export TF_VAR_cloudflare_token="op://common/cloudflare/credential"
 export TF_VAR_cloudflare_account_id="op://common/cloudflare/account-id"
 export TF_VAR_cloudflare_zone_id="op://common/cloudflare/zone-id"
-export TF_VAR_digitalocean_token="op://github-actions/digitalocean/credential"
-export TF_VAR_digitalocean_spaces_id="op://github-actions/digitalocean/spaces-access-key-id"
-export TF_VAR_digitalocean_spaces_key="op://github-actions/digitalocean/spaces-secret-access-key"
-export TF_VAR_vultr_token="op://github-actions/vultr/credential"
+export TF_VAR_digitalocean_token="op://common/digitalocean/credential"
+export TF_VAR_digitalocean_spaces_id="op://common/digitalocean/spaces-access-key-id"
+export TF_VAR_digitalocean_spaces_key="op://common/digitalocean/spaces-secret-access-key"
+export TF_VAR_vultr_token="op://common/vultr/credential"
 
 cd "$HOME/Repos/homescale/deploy/tofu"
 
