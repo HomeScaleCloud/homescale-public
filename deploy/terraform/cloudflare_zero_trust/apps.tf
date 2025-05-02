40848744c4e7dacde95ae2a08da368c943dbba90
Maxwell Morris <git@maxmorris.io>
2025-05-02 12:14:46 +0100

    terraform things

---
+++ deploy/terraform/cloudflare_zero_trust/apps.tf
@@ -0,0 +1,42 @@
+resource "cloudflare_zero_trust_access_application" "rancher" {
+  name                        = "Rancher"
+  domain                      = "REDACTED/dashboard"
+  type                        = "self_hosted"
+  account_id                  = var.cloudflare_account_id
+  allow_authenticate_via_warp = true
+  allowed_idps                = [data.cloudflare_zero_trust_access_identity_provider.entra_id.identity_provider_id]
+  auto_redirect_to_identity   = true
+  app_launcher_visible        = true
+  skip_interstitial           = true
+  logo_url                    = "https://www.rancher.com/assets/img/brand-guidelines/assets/logos/png/cow/rancher-logo-cow-blue.png"
+
+  destinations = [
+    {
+      type = "public"
+      uri  = "REDACTED/dashboard"
+    },
+    {
+      type = "public"
+      uri  = "REDACTED/k8s/clusters"
+    },
+    {
+      type = "public"
+      uri  = "REDACTED/assets"
+    },
+    {
+      type = "public"
+      uri  = "REDACTED/api-ui"
+    },
+    {
+      type = "public"
+      uri  = "REDACTED/v3-public"
+    }
+  ]
+
+  policies = [
+    {
+      id         = cloudflare_zero_trust_access_policy.allow_admins.id
+      precedence = 1
+    },
+  ]
+}
