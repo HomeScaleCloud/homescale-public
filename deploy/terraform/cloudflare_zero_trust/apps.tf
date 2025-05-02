990c682823e97d8c6004badd1630d463d19152b9
Maxwell Morris <git@maxmorris.io>
2025-05-02 12:23:46 +0100

    fix

--- deploy/terraform/cloudflare_zero_trust/apps.tf
+++ deploy/terraform/cloudflare_zero_trust/apps.tf
@@ -26,10 +26,6 @@ resource "cloudflare_zero_trust_access_application" "rancher" {
     {
       type = "public"
       uri  = "xxx/api-ui"
-    },
-    {
-      type = "public"
-      uri  = "xxx/v3-public"
     }
   ]
 
