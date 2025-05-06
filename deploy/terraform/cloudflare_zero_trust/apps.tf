d0be82d74825b5845227bf2d05266a66e691999d
Maxwell Morris <git@maxmorris.io>
2025-05-06 23:10:41 +0100

    fix

--- deploy/terraform/cloudflare_zero_trust/apps.tf
+++ deploy/terraform/cloudflare_zero_trust/apps.tf
@@ -21,10 +21,6 @@ resource "cloudflare_zero_trust_access_application" "rancher" {
     },
     {
       type = "public"
-      uri  = "rancher.homescale.cloud/assets"
-    },
-    {
-      type = "public"
       uri  = "rancher.homescale.cloud/api-ui"
     }
   ]
