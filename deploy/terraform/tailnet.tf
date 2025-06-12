069c750bba6cb4aa218ce6a8287a1d4518d61df5
Maxwell Morris <git@maxmorris.io>
2025-06-12 23:18:10 +0100

    fix

--- deploy/terraform/tailnet.tf
+++ deploy/terraform/tailnet.tf
@@ -11,7 +11,7 @@ resource "tailscale_acl" "acl" {
             },
             {
                 "action": "accept",
-                "src":    ["group:All Users@homescale.cloud"],
+                "src":    ["autogroup:member"],
                 "dst":    ["tag:app:*"],
             },
             {
