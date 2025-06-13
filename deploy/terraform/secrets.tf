d2667638d83d31721852ebd83645b9a1e7456420
Maxwell Morris <git@maxmorris.io>
2025-06-13 17:45:54 +0100

    fix???

--- deploy/terraform/secrets.tf
+++ deploy/terraform/secrets.tf
@@ -32,14 +32,14 @@ data "onepassword_item" "tailscale" {
 }
 
 resource "onepassword_item" "tailscale_oauth_github_actions" {
-  vault    = "github-actions"
+  vault    = data.onepassword_vault.github_actions.uuid
   title    = "tailscale"
   username = tailscale_oauth_client.github_actions.id
   password = tailscale_oauth_client.github_actions.key
 }
 
 resource "onepassword_item" "tailscale_oauth_operator_core" {
-  vault    = "core"
+  vault    = data.onepassword_vault.core.uuid
   title    = "tailscale"
   username = tailscale_oauth_client.operator_core.id
   password = tailscale_oauth_client.operator_core.key
