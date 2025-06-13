86b3e30b31c24058fa6cfbf9886d8edbeae6a059
Maxwell Morris <git@maxmorris.io>
2025-06-13 15:13:57 +0100

    fixes

--- deploy/terraform/tailnet.tf
+++ deploy/terraform/tailnet.tf
@@ -5,7 +5,7 @@ resource "tailscale_acl" "acl" {
         "acls": [
             {
                 "action":     "accept",
-                "src":        ["group:Admins@REDACTED"],
+                "src":        ["group:admins@REDACTED"],
                 "dst":        ["tag:admin-app:*"],
                 "srcPosture": ["posture:linux", "posture:macos", "posture:windows"],
             },
