b5c323602b0c77fd4757947484f5d93f52f2b837
Maxwell Morris <git@maxmorris.io>
2025-07-24 16:13:51 +0100

    fix

--- tools/hsctl_modules/cluster/standup.sh
+++ tools/hsctl_modules/cluster/standup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Script to provision/standup k8s clusters
-set -e
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <clusterName>"
@@ -24,8 +23,8 @@ check_op_logged_in() {
         SESSION_TOKEN=$(/usr/bin/op signin --raw)
 
         if [[ -z "$SESSION_TOKEN" ]]; then
-        echo "❌ Failed to sign in to 1Password." >&2
-        exit 1
+            echo "❌ Failed to sign in to 1Password." >&2
+            exit 1
         fi
 
         echo "$SESSION_TOKEN" > "$SESSION_FILE"
@@ -33,10 +32,16 @@ check_op_logged_in() {
         export OP_SESSION_my="$SESSION_TOKEN"
     fi
 }
+
 cluster_standup () {
     kubectl create ns argocd
-    kubectl apply -n argocd -k apps/argocd/overlays/$CLUSTER_NAME/manifests
-    kubectl apply -n argocd -f clusters/$CLUSTER_NAME
+
+    if ! kubectl apply -n argocd -k "apps/argocd/overlays/$CLUSTER_NAME/manifests"; then
+        echo "⚠️OnePasswordItem CRD not installed yet—continuing anyway."
+    fi
+
+    kubectl apply -n argocd -f "clusters/$CLUSTER_NAME"
+
     check_op_logged_in
 
     if ! /usr/bin/op vault get "$CLUSTER_NAME" >/dev/null 2>&1; then
@@ -47,20 +52,26 @@ cluster_standup () {
     RESPONSE=$(/usr/bin/op connect server create "$CLUSTER_NAME" --vaults "$CLUSTER_NAME,common")
     CONNECT_CREDENTIALS_FILE=$(echo "$RESPONSE" | grep "Credentials file" | awk '{print $NF}')
 
-    OPERATOR_TOKEN=$(/usr/bin/op connect token create "$CLUSTER_NAME" --server "$CLUSTER_NAME" --vault "$CLUSTER_NAME" --vault "common")
+    OPERATOR_TOKEN=$(/usr/bin/op connect token create "$CLUSTER_NAME" \
+        --server "$CLUSTER_NAME" --vault "$CLUSTER_NAME" --vault "common")
 
     CONNECT_CREDENTIALS=$(base64 -w 0 "$CONNECT_CREDENTIALS_FILE")
 
-    /usr/bin/op item create --vault "$CLUSTER_NAME" --category "API Credential" --title "onepassword" \
-        operator-token="$OPERATOR_TOKEN" connect-credentials="$CONNECT_CREDENTIALS"
+    /usr/bin/op item create --vault "$CLUSTER_NAME" --category "API Credential" \
+        --title "onepassword" \
+        operator-token="$OPERATOR_TOKEN" \
+        connect-credentials="$CONNECT_CREDENTIALS"
 
-    rm $CONNECT_CREDENTIALS_FILE
+    rm -f "$CONNECT_CREDENTIALS_FILE"
 
     kubectl create ns onepassword
     kubectl create secret generic "op-onepassword" \
-    --from-literal=operator-token="$OPERATOR_TOKEN" \
-    --from-literal=connect-credentials="$CONNECT_CREDENTIALS" \
-    -n onepassword
-    kubectl delete pods --all -n onepassword
+        --from-literal=operator-token="$OPERATOR_TOKEN" \
+        --from-literal=connect-credentials="$CONNECT_CREDENTIALS" \
+        -n onepassword
+
+    kubectl apply -n argocd -k apps/onepassword/overlays/$CLUSTER_NAME/manifests
     kubectl delete pods --all -n argocd
 }
+
+cluster_standup
