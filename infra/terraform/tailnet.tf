7b8097fcfef049d4f98c2c550cb09b8d025eb4b6
Maxwell Morris <git@maxmorris.io>
2025-08-18 12:11:23 +0100

    increase Tailscale re-auth window

--- infra/terraform/tailnet.tf
+++ infra/terraform/tailnet.tf
@@ -103,7 +103,7 @@ resource "tailscale_acl" "acl" {
 
 resource "tailscale_tailnet_settings" "settings" {
   devices_auto_updates_on                     = true
-  devices_key_duration_days                   = 18
+  devices_key_duration_days                   = 35
   posture_identity_collection_on              = true
   users_role_allowed_to_join_external_tailnet = "admin"
 }
