ae93666c3029e3375691a1297375210678dae01a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-09-01 15:18:03 +0100

    bare things

---
+++ infra/terraform/modules/cluster/variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "Region name to deploy to"
+  type        = string
+  default = "dev"
+  validation {
+    condition     = can(regex("^(dev|[a-z]{3}[0-9]{1,2})$", var.region))
+    error_message = "Region must be 'dev' or 3 lowercase letters followed by 1 or 2 digits (e.g., boa1, lon21)."
+  }
+}
+
+variable "kubeconfig" {
+  description = "Kubeconfig to use for chart deployment"
+  type        = string
+  sensitive   = true
+}
+
+variable "op_service_account_token" {
+  description = "1Password Service Account Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "tailscale_oauth_client_id" {
+  description = "Tailscale OAuth Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "tailscale_oauth_client_secret" {
+  description = "Tailscale OAuth Client Secret"
+  type        = string
+  sensitive   = true
+}
