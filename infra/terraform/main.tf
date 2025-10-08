e04d1137a5c1e82ef105deea8d9f5e5c1d63a55d
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-08 18:48:13 +0100

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,7 +3,7 @@ module "cluster_atlas" {
   cluster                       = "atlas"
   region                        = "boa1"
   controlplane_nodes            = ["10.1.245.5"]
-  controlplane_vip                           = "10.1.245.15"
+  controlplane_vip              = "10.1.245.15"
   talos_version                 = "1.11.2"
   tailscale_oauth_client_id     = data.onepassword_item.tailscale.username
   tailscale_oauth_client_secret = data.onepassword_item.tailscale.password
