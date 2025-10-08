c09edb797a57930d4209d6afbf06ba46c33ad80f
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-08 17:50:21 +0100

    fixes

--- infra/terraform/modules/cluster/talos.tf
+++ infra/terraform/modules/cluster/talos.tf
@@ -29,6 +29,13 @@ resource "talos_machine_configuration_apply" "controlplane" {
     }),
     yamlencode({
       machine = {
+        nodeLabels = {
+            "xxx/name" = var.cluster
+        }
+      }
+    }),
+    yamlencode({
+      machine = {
         install = { diskSelector = { size = 250059350016 } }
       }
     }),
