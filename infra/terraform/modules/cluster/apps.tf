2dcd7328d025e3979c9705a7a18f73e67a7a817b
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-09 23:35:54 +0100

    fix?

--- infra/terraform/modules/cluster/apps.tf
+++ infra/terraform/modules/cluster/apps.tf
@@ -297,7 +297,7 @@ locals {
 
 resource "kubernetes_manifest" "argocd_app" {
   for_each   = { for a in local.apps : a.releaseName => a }
-  depends_on = [helm_release.argocd]
+  depends_on = [talos_machine_bootstrap.controlplane, helm_release.argocd]
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "Application"
