b3fe43ac645d51c0a6d7d2eabd6cff93a0286f76
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-12 12:32:01 +0100

    deploy apps to atlas

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,5 @@
 module "cluster_atlas" {
   source                        = "./modules/cluster"
-  cluster_init                  = true
   cluster                       = "atlas"
   region                        = "boa1"
   platform                      = "metal"
