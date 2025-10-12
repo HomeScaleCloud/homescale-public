d3ca78fe70aee0a520acd6e789b49976a65d505e
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-12 12:22:50 +0100

    things

--- infra/terraform/modules/cluster/apps.tf
+++ infra/terraform/modules/cluster/apps.tf
@@ -292,7 +292,7 @@ locals {
       enabled = var.app_trivy_operator_enabled
     }
   ]
-  apps = [for a in local.apps_all : a if a.enabled]
+  apps = var.cluster_init ? [for a in local.apps_all : a if a.enabled] : []
 }
 
 resource "kubernetes_manifest" "argocd_app" {
