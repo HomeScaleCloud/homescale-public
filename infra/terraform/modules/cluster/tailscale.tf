f53f444859e4b55a3339b335a0992babfe70bca2
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-13 21:11:43 +0100

    fixes

--- infra/terraform/modules/cluster/tailscale.tf
+++ infra/terraform/modules/cluster/tailscale.tf
@@ -1,5 +1,5 @@
 resource "tailscale_oauth_client" "k8s_operator" {
   description = "k8s-${var.cluster}"
   scopes      = ["devices:core", "auth_keys"]
-  tags        = ["tag:k8s","tag:app","tag:cluster-${var.cluster}","tag:region-${var.region}"]
+  tags        = ["tag:k8s", "tag:app", "tag:cluster-${var.cluster}", "tag:region-${var.region}"]
 }
