c1b00fe2ce5437dce98e83e6043c1f6780af373f
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-13 15:41:15 +0100

    fix - https://github.com/tailscale/tailscale/issues/7868#issuecomment-1729967628

--- infra/terraform/modules/cluster/core_apps.tf
+++ infra/terraform/modules/cluster/core_apps.tf
@@ -3,45 +3,40 @@ resource "helm_release" "cilium" {
   depends_on = [
     talos_machine_bootstrap.controlplane
   ]
+
   name       = "cilium"
   repository = "https://helm.cilium.io/"
   chart      = "cilium"
   version    = "1.18.2"
   namespace  = "kube-system"
 
-  set = [
-    {
-      name  = "ipam.mode"
-      value = "kubernetes"
-    },
-    {
-      name  = "kubeProxyReplacement"
-      value = "true"
-    },
-    {
-      name  = "securityContext.capabilities.ciliumAgent"
-      value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}"
-    },
-    {
-      name  = "securityContext.capabilities.cleanCiliumState"
-      value = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}"
-    },
-    {
-      name  = "cgroup.autoMount.enabled"
-      value = "false"
-    },
-    {
-      name  = "cgroup.hostRoot"
-      value = "/sys/fs/cgroup"
-    },
-    {
-      name  = "k8sServiceHost"
-      value = "localhost"
-    },
-    {
-      name  = "k8sServicePort"
-      value = "7445"
-    }
+  values = [
+    yamlencode({
+      socketLB = {
+        socketLB.hostNamespaceOnly = true
+      }
+      bpf = {
+        bpf.lbExternalClusterIP = true
+      }
+      ipam = {
+        mode = "kubernetes"
+      }
+      kubeProxyReplacement = true
+      securityContext = {
+        capabilities = {
+          ciliumAgent       = ["CHOWN", "KILL", "NET_ADMIN", "NET_RAW", "IPC_LOCK", "SYS_ADMIN", "SYS_RESOURCE", "DAC_OVERRIDE", "FOWNER", "SETGID", "SETUID"]
+          cleanCiliumState  = ["NET_ADMIN", "SYS_ADMIN", "SYS_RESOURCE"]
+        }
+      }
+      cgroup = {
+        autoMount = {
+          enabled = false
+        }
+        hostRoot = "/sys/fs/cgroup"
+      }
+      k8sServiceHost = "localhost"
+      k8sServicePort = 7445
+    })
   ]
 }
 
