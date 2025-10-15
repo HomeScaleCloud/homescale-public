b321e9554bb76336ec9b514f3b3b06887b91ebaa
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-15 12:01:27 +0100

    things

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -9,7 +9,7 @@ module "cluster_atlas" {
   controlplane_nodes            = ["10.1.245.5"]
   workloads_on_controlplane     = true
   talos_version                 = "v1.11.2"
-  talos_disk_selector           = { size = 120034123776 }
+  talos_disk_selector           = { size = 525112713216 }
   tailscale_oauth_client_id     = data.onepassword_item.tailscale.username
   tailscale_oauth_client_secret = data.onepassword_item.tailscale.password
   op_service_account_token      = var.op_service_account_token
