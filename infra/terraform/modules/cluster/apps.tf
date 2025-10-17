33809b42bb7f569dd08ac3ff1835d6139f371048
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-17 22:41:01 +0100

    fix

--- infra/terraform/modules/cluster/apps.tf
+++ infra/terraform/modules/cluster/apps.tf
@@ -159,7 +159,7 @@ locals {
               root_url = "https://metrics.${var.cluster}.${var.region}.homescale.cloud"
             }
             auth = {
-              disable_login_form = false
+              disable_login_form = true
             }
             "auth.generic_oauth" = {
               enabled                    = true
