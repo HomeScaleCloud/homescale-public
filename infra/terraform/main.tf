5898370c615261a31c63fd4634f833aaf9e32de4
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-10-17 21:41:00 +0100

    undo

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,7 +8,6 @@ module "cluster_atlas" {
   controlplane_nodes            = ["10.1.245.5"]
   workloads_on_controlplane     = true
   talos_version                 = "v1.11.2"
-  store_talosconfig             = true
   talos_disk_selector           = { size = 525112713216 }
   app_nfs_provisioner_enabled   = true
   nfs_server                    = "10.1.245.4"
