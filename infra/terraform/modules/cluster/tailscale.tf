6f57ef25c0ab61c69f17a9bc245ff310ec4cb245
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-11-03 23:32:12 +0000

    things

--- infra/terraform/modules/cluster/tailscale.tf
+++ infra/terraform/modules/cluster/tailscale.tf
@@ -3,3 +3,12 @@ resource "tailscale_oauth_client" "k8s_operator" {
   scopes      = ["devices:core", "auth_keys"]
   tags        = ["tag:k8s", "tag:app", "tag:cluster-${var.cluster}", "tag:region-${var.region}"]
 }
+
+resource "tailscale_tailnet_key" "node" {
+  reusable      = true
+  ephemeral     = false
+  preauthorized = false
+  expiry        = 1200
+  description   = "Node Key"
+  tags          = ["tag:node", "tag:cluster-${var.cluster}", "tag:region-${var.region}"]
+}
