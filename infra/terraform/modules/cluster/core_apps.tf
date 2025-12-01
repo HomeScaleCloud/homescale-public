b68a88b1e7851c1d8277ec316a94aef7d25075c9
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-12-01 20:44:24 +0000

    typo

--- infra/terraform/modules/cluster/core_apps.tf
+++ infra/terraform/modules/cluster/core_apps.tf
@@ -248,7 +248,7 @@ resource "helm_release" "kro" {
   count      = var.init_stage_1 ? 0 : 1
   depends_on = [helm_release.cilium]
   name       = "kro"
-  repository = "oci://registry.k8s.io/kro/charts/kro"
+  repository = "oci://registry.k8s.io/kro/charts"
   chart      = "kro"
   version    = "0.4.1"
   namespace  = "kro"
