343ed4a0be1519e41b6e2904d3e1d77718685615
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2025-12-18 19:26:55 +0000

    fix

--- infra/terraform/tailnet.tf
+++ infra/terraform/tailnet.tf
@@ -116,13 +116,13 @@ resource "tailscale_acl" "acl" {
           "checkPeriod": "2h",
           "src": ["group:sec-infra-plat-pim@homescale.cloud"],
           "dst": ["tag:node"],
-          "users": ["admin"],
+          "users": ["hs"],
         },
         {
           "action": "accept",
           "src": ["tag:github-actions"],
           "dst": ["tag:node"],
-          "users": ["admin"],
+          "users": ["hs"],
         },
 
         // LF K8s Lab
@@ -131,9 +131,9 @@ resource "tailscale_acl" "acl" {
           "src": [
             "group:team-infra-plat@homescale.cloud",
             "popsbot@gmail.com"
-          ],
+          ]
           "dst": ["tag:lf-k8s-lab"],
-          "users": ["admin"],
+          "users": ["hs"],
         },
       ],
 
