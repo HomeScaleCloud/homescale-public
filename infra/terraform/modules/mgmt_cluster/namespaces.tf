a76c4124c7fcd02cc6a232fa4e75f5b27e7c055a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-02-25 23:24:50 +0000

    terraform fmt

--- infra/terraform/modules/mgmt_cluster/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/namespaces.tf
@@ -1,12 +1,12 @@
 resource "kubernetes_namespace" "onepassword" {
-  depends_on = [ digitalocean_kubernetes_cluster.mgmt ]
+  depends_on = [digitalocean_kubernetes_cluster.mgmt]
   metadata {
     name = "onepassword"
   }
 }
 
 resource "kubernetes_namespace" "tailscale" {
-  depends_on = [ digitalocean_kubernetes_cluster.mgmt ]
+  depends_on = [digitalocean_kubernetes_cluster.mgmt]
   metadata {
     name = "tailscale"
   }
