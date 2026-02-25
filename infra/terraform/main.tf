3732b3fc396bcaee47444345489b0494c6c2f4af
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-02-25 23:20:45 +0000

    migrate to Rancher's Fleet CD

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,10 +1,10 @@
-# module "cluster_atlas" {
-#   source                        = "./modules/cluster"
-#   cluster                       = "atlas"
-#   region                        = "boa1"
-#   platform                      = "metal"
-#   tailscale_tailnet             = "tempel-carp.ts.net"
-#   tailscale_oauth_client_id     = data.onepassword_item.tailscale.username
-#   tailscale_oauth_client_secret = data.onepassword_item.tailscale.password
-#   op_service_account_token      = var.op_service_account_token
-# }
+module "mgmt_cluster" {
+  source = "./modules/mgmt_cluster"
+  platform = "digitalocean"
+  region = "lon1"
+  version = "1.34.1-do.4"
+  op_service_account_token = var.op_service_account_token
+  tailscale_tailnet             = data.onepassword_item.tailscale_tailnet.credential
+  tailscale_oauth_client_id     = data.onepassword_item.tailscale.username
+  tailscale_oauth_client_secret = data.onepassword_item.tailscale.password
+}
