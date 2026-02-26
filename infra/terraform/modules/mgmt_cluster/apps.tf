9596e166fddd9979ce4d27cc85a1dbd597ee0ee1
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-02-26 23:51:29 +0000

    fixes

--- infra/terraform/modules/mgmt_cluster/apps.tf
+++ infra/terraform/modules/mgmt_cluster/apps.tf
@@ -1,11 +1,13 @@
 resource "helm_release" "rancher" {
   name              = "rancher"
+  namespace         = "cattle-system"
   chart             = "../../apps/rancher"
   dependency_update = true
 }
 
 resource "helm_release" "onepassword" {
   name              = "onepassword"
+  namespace         = "onepassword"
   chart             = "../../apps/onepassword"
   dependency_update = true
 }
