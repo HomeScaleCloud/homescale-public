32f77a2732783eb2638c57439a82ec4f6c95c7be
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-01 00:10:55 +0000

    things

---
+++ infra/terraform/modules/mgmt_cluster/fleet.tf
@@ -0,0 +1,67 @@
+resource "kubernetes_manifest_v1" "gitrepo_apps_prod" {
+  depends_on = [kubernetes_namespace_v1.prod]
+  manifest = {
+    apiVersion = "fleet.cattle.io/v1alpha1"
+    kind       = "GitRepo"
+    metadata = {
+      name      = "apps"
+      namespace = "prod"
+    }
+    spec = {
+      repo   = "https://github.com/HomeScaleCloud/homescale-public"
+      branch = "main"
+      paths  = ["apps/*"]
+    }
+  }
+}
+
+resource "kubernetes_manifest_v1" "gitrepo_apps_test" {
+  depends_on = [kubernetes_namespace_v1.test]
+  manifest = {
+    apiVersion = "fleet.cattle.io/v1alpha1"
+    kind       = "GitRepo"
+    metadata = {
+      name      = "apps"
+      namespace = "test"
+    }
+    spec = {
+      repo   = "https://github.com/HomeScaleCloud/homescale-public"
+      branch = "main"
+      paths  = ["apps/*"]
+    }
+  }
+}
+
+resource "kubernetes_manifest_v1" "gitrepo_apps_lab" {
+  depends_on = [kubernetes_namespace_v1.lab]
+  manifest = {
+    apiVersion = "fleet.cattle.io/v1alpha1"
+    kind       = "GitRepo"
+    metadata = {
+      name      = "apps"
+      namespace = "lab"
+    }
+    spec = {
+      repo   = "https://github.com/HomeScaleCloud/homescale-public"
+      branch = "main"
+      paths  = ["apps/*"]
+    }
+  }
+}
+
+resource "kubernetes_manifest_v1" "gitrepo_apps_mgmt" {
+  depends_on = [kubernetes_namespace_v1.lab]
+  manifest = {
+    apiVersion = "fleet.cattle.io/v1alpha1"
+    kind       = "GitRepo"
+    metadata = {
+      name      = "apps"
+      namespace = "fleet-local"
+    }
+    spec = {
+      repo   = "https://github.com/HomeScaleCloud/homescale-public"
+      branch = "main"
+      paths  = ["apps/*"]
+    }
+  }
+}
