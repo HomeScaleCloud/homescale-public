f8681b388a11c6037282755dfd492a5d1c1958f4
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-03 23:18:55 +0000

    fixes

--- infra/terraform/modules/mgmt_cluster/tailscale.tf
+++ infra/terraform/modules/mgmt_cluster/tailscale.tf
@@ -4,13 +4,22 @@ resource "tailscale_oauth_client" "k8s" {
   tags        = ["tag:k8s"]
 }
 
+resource "tailscale_tailnet_key" "node_mgmt" {
+  reusable      = true
+  ephemeral     = false
+  preauthorized = true
+  expiry        = 7776000
+  description   = "node-key-mgmt"
+  tags          = ["tag:node", "tag:node-mgmt", "tag:env-mgmt"]
+}
+
 resource "tailscale_tailnet_key" "node_prod" {
   reusable      = true
   ephemeral     = false
   preauthorized = true
   expiry        = 7776000
   description   = "node-key-prod"
-  tags          = ["tag:node", "tag:env-prod"]
+  tags          = ["tag:node", "tag:node-prod", "tag:env-prod"]
 }
 
 resource "tailscale_tailnet_key" "node_test" {
@@ -19,7 +28,7 @@ resource "tailscale_tailnet_key" "node_test" {
   preauthorized = true
   expiry        = 7776000
   description   = "node-key-test"
-  tags          = ["tag:node", "tag:env-test"]
+  tags          = ["tag:node", "tag:node-test", "tag:env-test"]
 }
 
 resource "tailscale_tailnet_key" "node_lab" {
@@ -28,5 +37,5 @@ resource "tailscale_tailnet_key" "node_lab" {
   preauthorized = true
   expiry        = 7776000
   description   = "node-key-lab"
-  tags          = ["tag:node", "tag:env-lab"]
+  tags          = ["tag:node", "tag:node-lab", "tag:env-lab"]
 }
