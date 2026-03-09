4fdf5eb08a7e694a85caaa3908be2a0f007a9c96
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-09 19:15:50 +0000

    setup rancher oidc

--- infra/terraform/modules/mgmt_cluster/rancher.tf
+++ infra/terraform/modules/mgmt_cluster/rancher.tf
@@ -10,3 +10,13 @@ resource "rancher2_bootstrap" "mgmt" {
   depends_on       = [data.kubernetes_secret_v1.rancher_bootstrap]
   initial_password = data.kubernetes_secret_v1.rancher_bootstrap.data["bootstrapPassword"]
 }
+
+resource "rancher2_auth_config_azuread" "azuread" {
+  application_id     = data.onepassword_item.rancher_oidc.username
+  application_secret = data.onepassword_item.rancher_oidc.password
+  auth_endpoint      = "https://login.microsoftonline.com/${data.onepassword_item.entra_tenant.credential}/oauth2/v2.0/authorize"
+  graph_endpoint     = "https://graph.microsoft.com"
+  rancher_url        = "https://mgmt.tempel-carp.ts.net/verify-auth-azure"
+  tenant_id          = data.onepassword_item.entra_tenant.credential
+  token_endpoint     = "https://login.microsoftonline.com/${data.onepassword_item.entra_tenant.credential}/oauth2/v2.0/token"
+}
