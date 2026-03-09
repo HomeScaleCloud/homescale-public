0da690db9ddada3c0179a1eed2a82c84fe84a957
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-09 19:15:50 +0000

    setup rancher oidc

--- infra/terraform/modules/mgmt_cluster/secrets.tf
+++ infra/terraform/modules/mgmt_cluster/secrets.tf
@@ -12,6 +12,11 @@ data "onepassword_item" "entra_tenant" {
   title = "entra-tenant"
 }
 
+data "onepassword_item" "rancher_oidc" {
+  vault = "k8s"
+  title = "rancher-oidc"
+}
+
 resource "kubernetes_secret_v1" "onepassword" {
   depends_on = [kubernetes_namespace_v1.onepassword]
   count      = var.bootstrapped ? 1 : 0
