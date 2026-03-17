53e769dd693dc624a7d16a4117ea082627b83d09
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-17 21:33:42 +0000

    set admin global role for infra-plat via pim

--- infra/terraform/modules/mgmt_cluster/rancher.tf
+++ infra/terraform/modules/mgmt_cluster/rancher.tf
@@ -22,3 +22,10 @@ resource "rancher2_auth_config_azuread" "entraid" {
   tenant_id          = data.onepassword_item.entra_tenant.credential
   token_endpoint     = "https://login.microsoftonline.com/${data.onepassword_item.entra_tenant.credential}/oauth2/v2.0/token"
 }
+
+resource "rancher2_global_role_binding" "sec_infra_plat_pim" {
+  depends_on         = [rancher2_auth_config_azuread.entraid]
+  name               = "sec-infra-plat-pim"
+  global_role_id     = "admin"
+  group_principal_id = "azuread_group://36561b94-c67f-4f42-b3ea-a17d0a5ff13e"
+}
