c3226425ed5a297547834186a84251be741ff43c
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-26 16:29:19 +0000

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,13 +1,10 @@
 module "mgmt_cluster" {
-  source                        = "./modules/mgmt_cluster"
-  digitalocean_token            = data.onepassword_item.digitalocean.credential
-  k8s_version                   = "1.34.5-do.0"
-  region                        = "lon1"
-  bootstrapped                  = true
-  op_service_account_token      = var.op_service_account_token
-  tailscale_tailnet             = data.onepassword_item.tailscale_tailnet.credential
-  tailscale_oauth_client_id     = data.onepassword_item.tailscale.username
-  tailscale_oauth_client_secret = data.onepassword_item.tailscale.password
+  source                   = "./modules/mgmt_cluster"
+  digitalocean_token       = data.onepassword_item.digitalocean.credential
+  k8s_version              = "1.34.5-do.0"
+  region                   = "lon1"
+  bootstrapped             = true
+  op_service_account_token = var.op_service_account_token
 }
 
 module "region_boa1" {
