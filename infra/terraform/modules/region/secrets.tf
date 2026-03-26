475233e7154c64203f492cff46e5e42f306da364
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-26 15:19:26 +0000

    fix

--- infra/terraform/modules/region/secrets.tf
+++ infra/terraform/modules/region/secrets.tf
@@ -8,7 +8,7 @@ data "onepassword_vault" "k8s" {
 }
 
 resource "onepassword_item" "twingate_mgmt" {
-  vault    = data.onepassword_vault.k8s.id
+  vault    = data.onepassword_vault.k8s.uuid
   title    = "twingate-connector-${var.region}-mgmt"
   password = twingate_connector_tokens.mgmt.access_token
 }
