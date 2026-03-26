76991cc8811403abd0477a49512cdadedf2d90fd
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-03-26 09:37:10 +0000

    fix

--- infra/terraform/modules/region/variables.tf
+++ infra/terraform/modules/region/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
   type        = string
 
   validation {
-    condition     = can(regex("^[A-Z]{3}[0-9]$", var.region))
+    condition     = can(regex("^[a-z]{3}[0-9]$", var.region))
     error_message = "Region must be in format AAA1 (e.g., BOA1, LON1, BRS1)."
   }
 }
