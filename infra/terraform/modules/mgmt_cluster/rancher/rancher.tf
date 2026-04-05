b3b554b55e07058745334b03da7e440f4fdbcecd
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-05 21:22:59 +0100

    patch

--- infra/terraform/modules/mgmt_cluster/rancher/rancher.tf
+++ infra/terraform/modules/mgmt_cluster/rancher/rancher.tf
@@ -7,10 +7,3 @@ resource "rancher2_auth_config_azuread" "entraid" {
   tenant_id          = data.onepassword_item.entra_tenant.credential
   token_endpoint     = "https://login.microsoftonline.com/${data.onepassword_item.entra_tenant.credential}/oauth2/v2.0/token"
 }
-
-resource "rancher2_global_role_binding" "sg_k8s_admin" {
-  depends_on         = [rancher2_auth_config_azuread.entraid]
-  name               = "sg-k8s-admin"
-  global_role_id     = "admin"
-  group_principal_id = "azuread_group://4d4167e8-204c-4328-b5f4-4dae634d46a8"
-}
