435adb964ed81323a0bb44aef5f573d2ff4299fa
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-05 21:01:45 +0100

    patch

--- infra/terraform/modules/mgmt_cluster/main.tf
+++ infra/terraform/modules/mgmt_cluster/main.tf
@@ -1,3 +1,4 @@
+# Enable one module per apply when standing up the mgmt cluster
 module "cluster" {
   source             = "./cluster"
   region             = "lon1"
@@ -5,14 +6,14 @@ module "cluster" {
   digitalocean_token = var.digitalocean_token
 }
 
-# module "bootstrap" {
-#   source                   = "./bootstrap"
-#   k8s_endpoint             = module.digitalocean_cluster.k8s_endpoint
-#   k8s_token                = module.digitalocean_cluster.k8s_token
-#   k8s_ca                   = module.digitalocean_cluster.k8s_ca
-#   twingate_token           = var.twingate_token
-#   op_service_account_token = var.op_service_account_token
-# }
+module "bootstrap" {
+  source                   = "./bootstrap"
+  k8s_endpoint             = module.digitalocean_cluster.k8s_endpoint
+  k8s_token                = module.digitalocean_cluster.k8s_token
+  k8s_ca                   = module.digitalocean_cluster.k8s_ca
+  twingate_token           = var.twingate_token
+  op_service_account_token = var.op_service_account_token
+}
 
 # module "rancher" {
 #   source = "./rancher"
