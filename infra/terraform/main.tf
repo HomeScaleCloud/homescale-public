f5ff37f6da5abd84aef109610ac0289015afae8d
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-05 16:53:06 +0100

    things

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,7 +3,6 @@ module "mgmt_cluster" {
   digitalocean_token       = data.onepassword_item.digitalocean.credential
   k8s_version              = "1.34."
   region                   = "lon1"
-  bootstrapped             = false
   op_service_account_token = var.op_service_account_token
   twingate_token           = data.onepassword_item.twingate.credential
 }
--- infra/terraform/modules/mgmt_cluster/rancher/fleet.tf
+++ infra/terraform/modules/mgmt_cluster/rancher/fleet.tf
@@ -1,6 +1,5 @@
 resource "kubernetes_manifest" "gitrepo_apps_prod" {
   depends_on = [kubernetes_namespace_v1.prod]
-  count      = var.bootstrapped ? 1 : 0
   manifest = {
     apiVersion = "fleet.cattle.io/v1alpha1"
     kind       = "GitRepo"
@@ -25,38 +24,13 @@ resource "kubernetes_manifest" "gitrepo_apps_prod" {
 
 resource "kubernetes_manifest" "gitrepo_apps_lab" {
   depends_on = [kubernetes_namespace_v1.lab]
-  count      = var.bootstrapped ? 1 : 0
+
   manifest = {
     apiVersion = "fleet.cattle.io/v1alpha1"
     kind       = "GitRepo"
     metadata = {
       name      = "apps"
       namespace = "lab"
-    }
-    spec = {
-      repo            = "https://github.com/HomeScaleCloud/homescale-public"
-      branch          = "main"
-      paths           = ["apps/*"]
-      pollingInterval = "3m"
-      correctDrift = {
-        enabled = true
-      }
-    }
-  }
-  field_manager {
-    force_conflicts = true
-  }
-}
-
-resource "kubernetes_manifest" "gitrepo_apps_mgmt" {
-  depends_on = [digitalocean_kubernetes_cluster.mgmt]
-  count      = var.bootstrapped ? 1 : 0
-  manifest = {
-    apiVersion = "fleet.cattle.io/v1alpha1"
-    kind       = "GitRepo"
-    metadata = {
-      name      = "apps"
-      namespace = "fleet-local"
     }
     spec = {
       repo            = "https://github.com/HomeScaleCloud/homescale-public"
