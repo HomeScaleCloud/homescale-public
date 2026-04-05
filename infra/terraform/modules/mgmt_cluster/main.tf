0ff7dd2ff99adc8e6d811288da2073d0d5c19197
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-05 20:27:47 +0100

    patch

--- infra/terraform/modules/mgmt_cluster/main.tf
+++ infra/terraform/modules/mgmt_cluster/main.tf
@@ -5,14 +5,14 @@ module "digitalocean_cluster" {
   digitalocean_token = var.digitalocean_token
 }
 
-# module "bootstrap" {
-#   source                   = "./bootstrap"
-#   k8s_endpoint             = module.digitalocean_cluster.k8s_endpoint
-#   k8s_token                = module.digitalocean_cluster.k8s_token
-#   k8s_ca                   = module.digitalocean_cluster.k8s_ca
-#   twingate_token           = var.twingate_token
-#   op_service_account_token = var.op_service_account_token
-# }
+module "bootstrap" {
+  source                   = "./bootstrap"
+  k8s_endpoint             = module.digitalocean_cluster.k8s_endpoint
+  k8s_token                = module.digitalocean_cluster.k8s_token
+  k8s_ca                   = module.digitalocean_cluster.k8s_ca
+  twingate_token           = var.twingate_token
+  op_service_account_token = var.op_service_account_token
+}
 
 # module "rancher" {
 #   source = "./rancher"
