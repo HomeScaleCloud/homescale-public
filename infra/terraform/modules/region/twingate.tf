6b408a9ac70c5d09bdb28d05f4374502906dd296
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-17 23:17:03 +0100

    things

--- infra/terraform/modules/region/twingate.tf
+++ infra/terraform/modules/region/twingate.tf
@@ -12,3 +12,49 @@ resource "twingate_connector" "mgmt" {
 resource "twingate_connector_tokens" "mgmt" {
   connector_id = twingate_connector.mgmt.id
 }
+
+data "twingate_group" "team_infra_plat" {
+  id = "R3JvdXA6ODEyMzI2"
+}
+
+data "twingate_security_policy" "high_risk" {
+  name = "High Risk Policy"
+}
+
+resource "twingate_resource" "resource" {
+  name              = "boa1-mgmt"
+  address           = "10.1.245.0/24"
+  remote_network_id = twingate_remote_network.mgmt.id
+
+  security_policy_id = data.twingate_security_policy.high_risk.id
+  protocols = {
+    allow_icmp = true
+    tcp = {
+      policy = "RESTRICTED"
+      ports  = ["80", "443", "10443", "22"]
+    }
+    udp = {
+      policy = "RESTRICTED"
+    }
+  }
+
+  access_policy {
+    mode          = "AUTO_LOCK"
+    approval_mode = "MANUAL"
+    duration      = "8h"
+  }
+
+  access_group {
+    group_id           = data.twingate_group.team_infra_plat.id
+    security_policy_id = data.twingate_security_policy.high_risk.id
+    access_policy {
+      mode          = "AUTO_LOCK"
+      approval_mode = "MANUAL"
+      duration      = "8h"
+    }
+  }
+
+  access_service {
+    service_account_id = var.twingate_github_actions_service_account_id
+  }
+}
