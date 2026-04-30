eb2e2c8152929ba520e66e1adc8c2b163427861a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-30 14:47:15 +0100

    fix Terraform drift

--- infra/terraform/modules/mgmt_cluster/rancher/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/rancher/namespaces.tf
@@ -2,10 +2,24 @@ resource "kubernetes_namespace_v1" "prod" {
   metadata {
     name = "prod"
   }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
+  }
 }
 
 resource "kubernetes_namespace_v1" "lab" {
   metadata {
     name = "lab"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
   }
 }
