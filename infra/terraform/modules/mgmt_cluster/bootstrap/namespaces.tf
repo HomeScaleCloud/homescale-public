cef2137f8aee7a2bc3fa8d44e0b7a825d50e9b71
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-30 14:47:15 +0100

    fix Terraform drift

--- infra/terraform/modules/mgmt_cluster/bootstrap/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/bootstrap/namespaces.tf
@@ -2,10 +2,24 @@ resource "kubernetes_namespace_v1" "onepassword" {
   metadata {
     name = "onepassword"
   }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
+  }
 }
 
 resource "kubernetes_namespace_v1" "twingate" {
   metadata {
     name = "twingate"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
   }
 }
