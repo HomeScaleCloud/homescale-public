859932a4d95e12c16c46fad75d8c55b69fad729d
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-04-30 13:17:29 +0100

    deprecating hsctl

--- tools/hsctl_modules/k8s/cleanup-namespace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Removes finalizers from stubborn namespaces that won't delete
-NAMESPACE=$1
-
-kubectl get namespace "$NAMESPACE" -o json \
-  | tr -d "\n" \
-  | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
-  | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
