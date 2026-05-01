684fba5c20f920b1d61a9d5e0adb88bfe14ac4fc
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-02 00:18:20 +0100

    things

--- infra/terraform/modules/mgmt_cluster/bootstrap_2/fleet.tf
+++ infra/terraform/modules/mgmt_cluster/bootstrap_2/fleet.tf
@@ -1,8 +1,3 @@
-resource "rancher2_bootstrap" "mgmt" {
-  depends_on       = [data.kubernetes_secret_v1.rancher_bootstrap]
-  initial_password = data.kubernetes_secret_v1.rancher_bootstrap.data["bootstrapPassword"]
-}
-
 resource "kubernetes_manifest" "gitrepo_apps_mgmt" {
   depends_on = [helm_release.rancher]
   manifest = {
