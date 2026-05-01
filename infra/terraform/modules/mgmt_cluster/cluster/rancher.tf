26e2da5d48fd550ce8068de3c7163b8a243be0fa
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-02 00:07:28 +0100

    revert

--- infra/terraform/modules/mgmt_cluster/cluster/rancher.tf
+++ infra/terraform/modules/mgmt_cluster/cluster/rancher.tf
@@ -17,27 +17,3 @@ resource "helm_release" "rancher" {
     })
   ]
 }
-
-resource "kubernetes_manifest" "gitrepo_apps_mgmt" {
-  depends_on = [helm_release.rancher]
-  manifest = {
-    apiVersion = "fleet.cattle.io/v1alpha1"
-    kind       = "GitRepo"
-    metadata = {
-      name      = "apps-mgmt"
-      namespace = "fleet-local"
-    }
-    spec = {
-      repo            = "https://github.com/HomeScaleCloud/homescale"
-      branch          = "main"
-      paths           = ["apps/*"]
-      pollingInterval = "3m"
-      correctDrift = {
-        enabled = true
-      }
-    }
-  }
-  field_manager {
-    force_conflicts = true
-  }
-}
