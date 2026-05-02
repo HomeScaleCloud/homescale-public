62730f347ae9a05fed5cbfcbb2d81b06ad655c3b
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-02 14:20:17 +0100

    fix

--- infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
@@ -10,3 +10,29 @@ resource "kubernetes_namespace_v1" "onepassword" {
     ]
   }
 }
+
+resource "kubernetes_namespace_v1" "prod" {
+  metadata {
+    name = "prod"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
+  }
+}
+
+resource "kubernetes_namespace_v1" "lab" {
+  metadata {
+    name = "lab"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].annotations["cattle.io/status"],
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+    ]
+  }
+}
