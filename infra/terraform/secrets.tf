9c98472336b5ebf6d9bee66b49ff15c2428733af
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-02 09:17:29 +0100

    things

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -25,14 +25,3 @@ data "onepassword_item" "digitalocean" {
   vault = "github-actions"
   title = "digitalocean"
 }
-
-data "onepassword_item" "twingate" {
-  vault = "github-actions"
-  title = "twingate"
-}
-
-resource "onepassword_item" "twingate_github_actions_service_key" {
-  vault    = data.onepassword_vault.github_actions.uuid
-  title    = "twingate-service-key"
-  password = twingate_service_account_key.github_actions.token
-}
