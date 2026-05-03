6691f0a51a01b3f248ac04a656157f291f82c6c6
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-03 22:35:13 +0100

    remove ziti

--- infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
@@ -11,19 +11,6 @@ resource "kubernetes_namespace_v1" "onepassword" {
   }
 }
 
-resource "kubernetes_namespace_v1" "ziti" {
-  metadata {
-    name = "ziti"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].annotations["cattle.io/status"],
-      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
-    ]
-  }
-}
-
 resource "kubernetes_namespace_v1" "prod" {
   metadata {
     name = "prod"
