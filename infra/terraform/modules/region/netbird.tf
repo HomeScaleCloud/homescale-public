65544d2bb779d376c5c15221f216b87a3b0b1c98
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-11 23:17:34 +0100

    things

--- infra/terraform/modules/region/netbird.tf
+++ infra/terraform/modules/region/netbird.tf
@@ -51,3 +51,13 @@ resource "netbird_network_resource" "bmc" {
   groups     = [netbird_group.region_bmc.id, data.netbird_group.env_mgmt.id, data.netbird_group.net_region_bmc.id]
   enabled    = true
 }
+
+resource "netbird_setup_key" "region_router" {
+  name                   = "${var.region}-router"
+  expiry_seconds         = 86400
+  type                   = "one-off"
+  allow_extra_dns_labels = true
+  auto_groups            = [netbird_group.region_mgmt.id, netbird_group.region_bmc.id]
+  ephemeral              = false
+  usage_limit            = 3
+}
