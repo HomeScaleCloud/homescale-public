fc5cbd90c3a4eadd24a693200eb5ee95a6499d22
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-11 23:17:34 +0100

    things

--- infra/terraform/modules/region/secrets.tf
+++ infra/terraform/modules/region/secrets.tf
@@ -1,3 +1,9 @@
 data "onepassword_vault" "k8s" {
   name = "k8s"
 }
+
+resource "onepassword_item" "netbird_region_setup_key" {
+  vault    = data.onepassword_vault.k8s.uuid
+  title    = "netbird-${var.region}-setup-key"
+  password = netbird_setup_key.region_router.key
+}
