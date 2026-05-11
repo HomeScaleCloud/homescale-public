141792de46ca14624d23882d793683c177abe32e
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-11 23:48:58 +0100

    things

--- infra/terraform/modules/netbird/policies.tf
+++ infra/terraform/modules/netbird/policies.tf
@@ -12,6 +12,36 @@ resource "netbird_policy" "lab" {
   }
 }
 
+resource "netbird_policy" "region_mgmt" {
+  name    = "Region Mgmt Networks"
+  enabled = true
+  rule {
+    action        = "accept"
+    bidirectional = false
+    enabled       = true
+    protocol      = "tcp"
+    ports         = ["80", "443", "8443", "22"]
+    name          = "Region Mgmt Networks"
+    sources       = [netbird_group.github_actions.id, data.netbird_group.sg_k8s_admin.id]
+    destinations  = [data.netbird_group.net_region_mgmt.id]
+  }
+}
+
+resource "netbird_policy" "region_bmc" {
+  name    = "Region BMC Networks"
+  enabled = true
+  rule {
+    action        = "accept"
+    bidirectional = false
+    enabled       = true
+    protocol      = "tcp"
+    ports         = ["80", "443", "8443", "22"]
+    name          = "Region BMC Networks"
+    sources       = [netbird_group.github_actions.id, data.netbird_group.sg_k8s_admin.id]
+    destinations  = [data.netbird_group.net_region_bmc.id]
+  }
+}
+
 resource "netbird_policy" "rancher" {
   name    = "Rancher"
   enabled = true
