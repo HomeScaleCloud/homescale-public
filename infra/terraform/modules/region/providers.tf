e40d9019762b0562e7353b695332e055fb923864
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-11 23:13:18 +0100

    things

--- infra/terraform/modules/region/providers.tf
+++ infra/terraform/modules/region/providers.tf
@@ -3,9 +3,16 @@ terraform {
     onepassword = {
       source = "1Password/onepassword"
     }
+    netbird = {
+      source = "netbirdio/netbird"
+    }
   }
 }
 
 provider "onepassword" {
   service_account_token = var.op_service_account_token
+}
+
+provider "netbird" {
+  token = var.netbird_token
 }
