844f87c23cd5e6aa90fabd55224a4d9a13fd7335
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-12 12:56:12 +0100

    thing

--- infra/terraform/modules/netbird/policies.tf
+++ infra/terraform/modules/netbird/policies.tf
@@ -53,6 +53,10 @@ resource "netbird_policy" "region_router_ssh" {
     name          = "Region Router SSH"
     sources       = [netbird_group.github_actions.id, data.netbird_group.sg_ssh_admin.id]
     destinations  = [netbird_group.region_routers.id]
+    authorized_groups = {
+      (data.netbird_group.sg_ssh_admin.id) = ["admin"]
+      (netbird_group.github_actions.id)    = ["admin"]
+    }
   }
 }
 
