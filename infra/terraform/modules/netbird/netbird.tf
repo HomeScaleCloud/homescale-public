2617caba0f242987b86f1d93fbc4d61b7d3ebfde
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-12 11:37:25 +0100

    fix

--- infra/terraform/modules/netbird/netbird.tf
+++ infra/terraform/modules/netbird/netbird.tf
@@ -1,5 +1,5 @@
 resource "netbird_account_settings" "settings" {
-  peer_login_expiration              = 86400
+  peer_login_expiration              = 345600
   peer_login_expiration_enabled      = true
   peer_inactivity_expiration_enabled = false
   regular_users_view_blocked         = true
