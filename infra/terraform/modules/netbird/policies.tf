397e793ce89ea791a34033784da6388a4ad8177c
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-12 17:42:36 +0100

    fixes

--- infra/terraform/modules/netbird/policies.tf
+++ infra/terraform/modules/netbird/policies.tf
@@ -42,24 +42,6 @@ resource "netbird_policy" "region_bmc" {
   }
 }
 
-resource "netbird_policy" "region_router_ssh" {
-  name    = "Region Router SSH"
-  enabled = true
-  rule {
-    action        = "accept"
-    bidirectional = false
-    enabled       = true
-    protocol      = "netbird-ssh"
-    name          = "Region Router SSH"
-    sources       = [netbird_group.github_actions.id, data.netbird_group.sg_ssh_admin.id]
-    destinations  = [netbird_group.region_routers.id]
-    authorized_groups = {
-      (data.netbird_group.sg_ssh_admin.id) = ["admin"]
-      (netbird_group.github_actions.id)    = ["admin"]
-    }
-  }
-}
-
 resource "netbird_policy" "rancher" {
   name    = "Rancher"
   enabled = true
