497ad4adfa10bc78da1c1ea46ed1bd88814afceb
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-13 00:04:05 +0100

    fixes?

--- infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
+++ infra/terraform/modules/mgmt_cluster/bootstrap_2/namespaces.tf
@@ -24,19 +24,6 @@ resource "kubernetes_namespace_v1" "prod" {
   }
 }
 
-resource "kubernetes_namespace_v1" "metal" {
-  metadata {
-    name = "metal"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].annotations["cattle.io/status"],
-      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
-    ]
-  }
-}
-
 resource "kubernetes_namespace_v1" "lab" {
   metadata {
     name = "lab"
