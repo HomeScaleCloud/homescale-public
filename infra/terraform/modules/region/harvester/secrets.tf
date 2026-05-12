22dc20dda5f93ea4ac4699892a18b9c5bed16d92
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-05-12 20:35:12 +0100

    mah

--- infra/terraform/modules/region/harvester/secrets.tf
+++ infra/terraform/modules/region/harvester/secrets.tf
@@ -2,8 +2,3 @@ data "onepassword_item" "harvester_kubeconfig" {
   vault = "k8s"
   title = "metal-${var.region}-kubeconfig"
 }
-
-data "onepassword_item" "rancher" {
-  vault = "github-actions"
-  title = "rancher"
-}
