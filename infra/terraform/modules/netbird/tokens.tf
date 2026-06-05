b290fd432eb41b316d1e848bc07b692b21c2c7f0
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-05 23:23:39 +0100

    things

--- infra/terraform/modules/netbird/tokens.tf
+++ infra/terraform/modules/netbird/tokens.tf
@@ -3,13 +3,3 @@ resource "netbird_token" "k8s_operator" {
   name            = "Kubernetes Operator"
   expiration_days = 30
 }
-
-resource "netbird_setup_key" "github_actions" {
-  name                   = "GitHub Actions"
-  expiry_seconds         = 86400
-  type                   = "reusable"
-  allow_extra_dns_labels = true
-  auto_groups            = [netbird_group.github_actions.id]
-  ephemeral              = true
-  usage_limit            = 0
-}
