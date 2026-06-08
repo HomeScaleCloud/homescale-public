7901e6f0c6479b12aae868a70f02215ebb6f9ef0
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-09 00:26:47 +0100

    revert

--- infra/terraform/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.mgmt_cluster.module.bootstrap.kubernetes_namespace_v1.argocd
-  id = "argocd"
-}
-
-import {
-  to = module.mgmt_cluster.module.bootstrap.kubernetes_namespace_v1.infisical
-  id = "infisical"
-}
