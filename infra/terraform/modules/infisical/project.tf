2fc15d8fbcca32452d9515714a30b4bd9cb3cfbc
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-08 21:56:57 +0100

    things

--- infra/terraform/modules/infisical/project.tf
+++ infra/terraform/modules/infisical/project.tf
@@ -1,17 +1,3 @@
-resource "infisical_project" "homescale" {
-  name                       = "homescale"
-  slug                       = "homescale"
-  should_create_default_envs = false
-}
-
-resource "infisical_project_environment" "production" {
-  project_id = infisical_project.homescale.id
-  name       = "production"
-  slug       = "production"
-}
-
-resource "infisical_project_environment" "development" {
-  project_id = infisical_project.homescale.id
-  name       = "development"
-  slug       = "development"
+data "infisical_projects" "homescale" {
+  slug = "homescale"
 }
