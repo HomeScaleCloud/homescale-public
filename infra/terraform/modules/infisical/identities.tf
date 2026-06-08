87a2d3a14dfffe7c9d58163e154841a47b18593d
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-09 00:03:10 +0100

    things

--- infra/terraform/modules/infisical/identities.tf
+++ infra/terraform/modules/infisical/identities.tf
@@ -1,11 +1,11 @@
-resource "infisical_identity" "k8s_mgmt_operator" {
-  name   = "k8s-mgmt-operator"
+resource "infisical_identity" "k8s_operator" {
+  name   = "k8s-operator"
   org_id = var.org_id
   role   = "no-access"
 }
 
-resource "infisical_identity_universal_auth" "k8s_mgmt_operator" {
-  identity_id          = infisical_identity.k8s_mgmt_operator.id
+resource "infisical_identity_universal_auth" "k8s_operator" {
+  identity_id          = infisical_identity.k8s_operator.id
   access_token_ttl     = 3600
   access_token_max_ttl = 86400
   access_token_trusted_ips = [
@@ -14,13 +14,13 @@ resource "infisical_identity_universal_auth" "k8s_mgmt_operator" {
   ]
 }
 
-resource "infisical_identity_universal_auth_client_secret" "k8s_mgmt_operator" {
-  identity_id = infisical_identity.k8s_mgmt_operator.id
+resource "infisical_identity_universal_auth_client_secret" "k8s_operator" {
+  identity_id = infisical_identity.k8s_operator.id
 }
 
-resource "infisical_project_identity" "k8s_mgmt_operator" {
+resource "infisical_project_identity" "k8s_operator" {
   project_id  = data.infisical_projects.homescale.id
-  identity_id = infisical_identity.k8s_mgmt_operator.id
+  identity_id = infisical_identity.k8s_operator.id
   roles = [{
     role_slug    = "viewer"
     is_temporary = false
--- infra/terraform/modules/infisical/outputs.tf
+++ infra/terraform/modules/infisical/outputs.tf
@@ -3,11 +3,11 @@ output "project_id" {
   value       = data.infisical_projects.homescale.id
 }
 
-output "k8s_mgmt_operator_client_id" {
-  value = infisical_identity_universal_auth_client_secret.k8s_mgmt_operator.client_id
+output "k8s_operator_client_id" {
+  value = infisical_identity_universal_auth_client_secret.k8s_operator.client_id
 }
 
-output "k8s_mgmt_operator_client_secret" {
-  value     = infisical_identity_universal_auth_client_secret.k8s_mgmt_operator.client_secret
+output "k8s_operator_client_secret" {
+  value     = infisical_identity_universal_auth_client_secret.k8s_operator.client_secret
   sensitive = true
 }
