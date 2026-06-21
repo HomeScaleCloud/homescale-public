41b73ea4bcdc8c637accf10eb1a35ac844786763
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-21 13:05:53 +0100

    things

--- infra/terraform/modules/netbird/dns.tf
+++ infra/terraform/modules/netbird/dns.tf
@@ -5,3 +5,19 @@ resource "netbird_dns_record" "omni" {
   content = "xxx"
   ttl     = 300
 }
+
+resource "netbird_dns_zone" "metrics" {
+  name                 = "xxx"
+  domain               = "xxx"
+  enabled              = true
+  enable_search_domain = false
+  distribution_groups  = [data.netbird_group.all.id]
+}
+
+resource "netbird_dns_record" "grafana" {
+  zone_id = netbird_dns_zone.metrics.id
+  name    = "xxx"
+  type    = "CNAME"
+  content = "xxx"
+  ttl     = 300
+}
