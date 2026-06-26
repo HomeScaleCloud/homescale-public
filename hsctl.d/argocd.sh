63f1c0f1c36ad4072fe3c7d4e6328ab50b6e659a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-26 23:07:41 +0100

    patch

--- hsctl.d/argocd.sh
+++ hsctl.d/argocd.sh
@@ -6,6 +6,7 @@ argocd_usage() {
     echo ""
     echo "Subcommands:"
     echo "  login <cluster>   Log in to the ArgoCD instance for a cluster via SSO"
+    echo "  open <cluster>    Open the ArgoCD UI for a cluster in the browser"
     exit 1
 }
 
@@ -16,12 +17,20 @@ argocd_login() {
     argocd login "argocd-server.argocd.${cluster}xxx" --sso
 }
 
+argocd_open() {
+    local cluster="${1:-}"
+    [[ -z "$cluster" ]] && { echo "Usage: hsctl argocd open <cluster>"; exit 1; }
+
+    open "https://argocd-server.argocd.${cluster}xxx"
+}
+
 argocd_main() {
     [[ $# -eq 0 ]] && argocd_usage
 
     local sub="$1"; shift
     case "$sub" in
         login) argocd_login "$@" ;;
+        open)  argocd_open  "$@" ;;
         *) echo "hsctl argocd: unknown subcommand '$sub'" >&2; argocd_usage ;;
     esac
 }
