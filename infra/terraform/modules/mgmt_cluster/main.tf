6bd58720c695fd46b321d1b834e080dc5ec74e6e
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-26 19:46:42 +0100

    things

--- infra/terraform/modules/mgmt_cluster/main.tf
+++
@@ -1,9 +0,0 @@
-module "bootstrap" {
-  source                               = "./bootstrap"
-  k8s_endpoint                         = digitalocean_kubernetes_cluster.mgmt.endpoint
-  k8s_token                            = digitalocean_kubernetes_cluster.mgmt.kube_config.0.token
-  k8s_ca                               = base64decode(digitalocean_kubernetes_cluster.mgmt.kube_config.0.cluster_ca_certificate)
-  infisical_k8s_operator_client_id     = var.infisical_k8s_operator_client_id
-  infisical_k8s_operator_client_secret = var.infisical_k8s_operator_client_secret
-  argocd_deploy_key                    = var.argocd_deploy_key
-}
--- infra/terraform/modules/mgmt_cluster/variables.tf
+++ infra/terraform/modules/mgmt_cluster/variables.tf
@@ -18,20 +18,3 @@ variable "infisical_workspace_id" {
   description = "Infisical project/workspace UUID"
   type        = string
 }
-
-variable "infisical_k8s_operator_client_id" {
-  description = "Client ID for the k8s-mgmt-operator Infisical identity"
-  type        = string
-}
-
-variable "infisical_k8s_operator_client_secret" {
-  description = "Client secret for the k8s-mgmt-operator Infisical identity"
-  type        = string
-  sensitive   = true
-}
-
-variable "argocd_deploy_key" {
-  description = "Base64-encoded SSH private key for ArgoCD to pull from GitHub"
-  type        = string
-  sensitive   = true
-}
