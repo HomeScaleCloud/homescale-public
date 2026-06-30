83ce7857d118cc184e67172df10d3a2650a7714f
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-06-30 16:43:53 +0100

    tidy

--- infra/terraform/modules/mgmt_cluster/variables.tf
+++ infra/terraform/modules/mgmt_cluster/variables.tf
@@ -21,7 +21,7 @@ variable "node_count" {
 }
 
 variable "node_plan" {
-  description = "Vultr plan ID for worker nodes (default: vc2-2c-4gb = 2 vCPU, 4 GB RAM)"
+  description = "Vultr plan ID for worker nodes"
   type        = string
   default     = "vc2-4c-8gb"
 }
