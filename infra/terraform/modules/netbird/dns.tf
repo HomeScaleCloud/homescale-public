42bd9a21774b4ce80bfced0bb088e79fd9e178f5
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2026-07-02 19:46:09 +0100

    things

--- infra/terraform/modules/netbird/dns.tf
+++ infra/terraform/modules/netbird/dns.tf
@@ -1,11 +1,3 @@
-resource "netbird_dns_record" "omni" {
-  zone_id = netbird_dns_zone.cluster["mgmt"].id
-  name    = "REDACTED"
-  type    = "CNAME"
-  content = "REDACTED"
-  ttl     = 300
-}
-
 resource "netbird_dns_record" "kubeapi" {
   for_each = netbird_dns_zone.cluster
 
